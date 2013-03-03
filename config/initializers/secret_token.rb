5296e24cc5870fa484d566fe95c1064d09fa3510
Tony Myer <mediamaker@gmail.com>
2013-03-03 11:31:39 +0100

    new_issues
    Created new_issues:
    rails new new_issues

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewIssues::Application.config.secret_token = 'acd29c1337db30df196bfa504249802a335eae756da5ca35c15402ef05134b181950256029f1dfb0a5c53c2ed300fb9d4a928ce4e5f2bfe8a5d2c126c6d8f3e3'
